825a9d2d1092241f0126a9eae16da9495c1855f6
CalebScott0 <calebbenscott@gmail.com>
2024-08-02 19:25:42 -0500

    converting json dataset files to JS for exporting and data handling

--- db/test.js
+++ db/test.js
@@ -3,4 +3,6 @@ const str = businesses;
 const lines = str.split(/\n/);
 const wrapped = "[" + lines.join(",") + "]";
 const obj = JSON.parse(wrapped);
-console.log(obj.length());
+for (let i = 0; i <= 1; i++) {
+  console.log(obj[i]);
+}
