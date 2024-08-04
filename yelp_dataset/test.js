a4a6e6ef8840484b95e751106e1403d3e511319b
CalebScott0 <calebbenscott@gmail.com>
2024-08-03 23:37:27 -0500

    testing upload of first git lfs file with tracking tag

--- yelp_dataset/test.js
+++ yelp_dataset/test.js
@@ -1,7 +1,29 @@
-const { businesses_array } = require("./dataset_business");
+const { review_array1 } = require("./review_dataset/review_1dataset");
+const { review_array2 } = require("./review_dataset/review_2dataset");
+const { review_array3 } = require("./review_dataset/review_3dataset");
+const { review_array4 } = require("./review_dataset/review_4dataset");
+const { review_array5 } = require("./review_dataset/review_5dataset");
+const { review_array6 } = require("./review_dataset/review_6dataset");
+const { review_array7 } = require("./review_dataset/review_7dataset");
+const { review_array8 } = require("./review_dataset/review_8dataset");
+const { review_array9 } = require("./review_dataset/review_9dataset");
+const { review_array10 } = require("./review_dataset/review_10dataset");
 
-console.log(businesses_array[0].name, businesses_array[0]);
+const reviewArr = [
+  ...review_array1,
+  ...review_array2,
+  ...review_array3,
+  ...review_array4,
+  ...review_array5,
+  ...review_array6,
+  ...review_array7,
+  ...review_array8,
+  ...review_array9,
+  ...review_array10,
+];
+console.log("First Object index 0: ", reviewArr[0]);
 
-const randNum = Math.floor(Math.random() * businesses_array.length);
-console.log(businesses_array[randNum].name, businesses_array[randNum]);
-console.log("index of second business: ", randNum);
+const randNum = Math.floor(Math.random() * reviewArr.length);
+console.log(reviewArr[randNum].review_id, reviewArr[randNum]);
+console.log("index of second object: ", randNum);
+console.log("Array length: ", reviewArr.length);
