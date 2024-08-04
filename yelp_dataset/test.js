52f19316c3bad5f8b7b857d4007a5d076e3fbd6c
CalebScott0 <calebbenscott@gmail.com>
2024-08-03 21:21:55 -0500

    formatted test.js to test first index of array converted from ndjson and a random index (0-array length)

--- yelp_dataset/test.js
+++ yelp_dataset/test.js
@@ -2,5 +2,6 @@ const { businesses_array } = require("./dataset_business");
 
 console.log(businesses_array[0].name, businesses_array[0]);
 
-const randNum = Math.floor(Math.random() * businesses_array.length - 1);
+const randNum = Math.floor(Math.random() * businesses_array.length);
 console.log(businesses_array[randNum].name, businesses_array[randNum]);
+console.log("index of second business: ", randNum);
