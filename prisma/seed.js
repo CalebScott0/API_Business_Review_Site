1c57b055fba00bee3cd810cf0c94666034d78f47
CalebScott0 <calebbenscott@gmail.com>
2024-08-04 22:05:20 -0500

    created array of unique categories

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,34 +1,17 @@
 const { PrismaClient } = require("@prisma/client");
 const { faker } = require("@prisma/client");
+const { businessArr } = require("../yelp_dataset/business_dataset");
+
 const prisma = new PrismaClient();
-const businesses = require("../yelp_dataset/dataset_business");
 
-async function main() {
-  for (let i = 0; i < businesses.length; i++) {
-    const { business_id, name, address, city, state, stars, review_count } =
-      businesses[i];
-
-    await prisma.business.create({
-      business_id,
-      name,
-      address,
-      city,
-      state,
-      stars,
-      review_count,
-    });
+const categoryArray = [];
+for (let i = 0; i < businessArr.length; i++) {
+  const splitCategories = businessArr[i].categories
+    ? businessArr[i].categories.split(", ")
+    : [];
+  for (let j = 0; j < splitCategories.length; j++) {
+    !categoryArray.includes(splitCategories[j]) &&
+      categoryArray.push(splitCategories[j]);
   }
-  const businessData = await prisma.business.findMany({
-    take: 10,
-  });
-  console.log("Business", businessData);
 }
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.log(e);
-    await prisma.$disconnect();
-  });
+console.log(categoryArray);
--- yelp_dataset/business_dataset.js
+++ yelp_dataset/business_dataset.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:422d4b13449eac148437ddaf8caab7ebd7d97edce599b92843caccb11dde4ad9
-size 118864078
+oid sha256:e9aa7d6de6f65550fa5fd320a48d7d0f17fa9e4f8a66d669e227e52f387cb4e1
+size 118864068
