197de2be43bda1d8302cecb9221e1fb5d0b6e603
CalebScott0 <calebbenscott@gmail.com>
2024-08-09 15:25:56 -0500

    seeding business categories

--- prisma/seed/seedCategories.js
+++ prisma/seed/seedCategories.js
@@ -6,19 +6,20 @@ const prisma = new PrismaClient();
 async function main() {
   console.log("Seeding business categories...");
   // change this to be all categories to show ranking by occurence in prisma query?
-  const categoryArray = [];
+  const categories = [];
   for (let i = 0; i < businessArr.length; i++) {
     // split categories into array if business has categories property
     const splitCategories = businessArr[i].categories
       ? businessArr[i].categories.split(", ")
       : [];
-    for (let j = 0; j < splitCategories.length; j++) {
-      // push category to array if not already included
-      !categoryArray.includes(splitCategories[j]) &&
-        categoryArray.push(splitCategories[j]);
-    }
+    categories.push(splitCategories[j]);
   }
-  console.log(categoryArray);
+  
+  // SEED CATEGORIES INTO CATEGORIES TABLE AND THEN SEED CATEGORIES TO BUSINESS TABLE ALL IN THIS FILE AT ONCE!!
+
+  //   USE ORDER BY ON RETURN IN PRISMA QUERY TO ORDER BY CATEGORIES
+  //  AND INCLUDE A COUNT OF EACH IN THE RESPONSE!!
+  console.log(categories[0]);
 }
 main()
   .then(async () => {
