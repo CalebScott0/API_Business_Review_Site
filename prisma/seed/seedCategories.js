c3ac77beb35a10928b097c8919ddfab11fb94682
CalebScott0 <calebbenscott@gmail.com>
2024-08-09 14:36:17 -0500

    seeding business categories

--- prisma/seed/seedCategories.js
+++ prisma/seed/seedCategories.js
@@ -1,13 +1,28 @@
-// change this to be all categories to show ranking by occurence in prisma query?
+const { PrismaClient } = require("@prisma/client");
+const { businessArr } = require("../../yelp_dataset/business_dataset");
 
-// const categoryArray = [];
-// for (let i = 0; i < businessArr.length; i++) {
-//   const splitCategories = businessArr[i].categories
-//     ? businessArr[i].categories.split(", ")
-//     : [];
-//   for (let j = 0; j < splitCategories.length; j++) {
-//     !categoryArray.includes(splitCategories[j]) &&
-//       categoryArray.push(splitCategories[j]);
-//   }
-// }
-// // console.log(categoryArray);
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log("Seeding business categories...");
+  // change this to be all categories to show ranking by occurence in prisma query?
+  // const categoryArray = [];
+  // for (let i = 0; i < businessArr.length; i++) {
+  //   const splitCategories = businessArr[i].categories
+  //     ? businessArr[i].categories.split(", ")
+  //     : [];
+  //   for (let j = 0; j < splitCategories.length; j++) {
+  //     !categoryArray.includes(splitCategories[j]) &&
+  //       categoryArray.push(splitCategories[j]);
+  //   }
+  // }
+  // // console.log(categoryArray);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.log(e);
+    await prisma.$disconnect();
+  });
