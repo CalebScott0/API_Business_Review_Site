020b057fac7b47c00a800ec3ea5d7d399e42c996
CalebScott0 <calebbenscott@gmail.com>
2024-08-08 21:42:07 -0500

    login and register routes running and tested

--- db/reviews.js
+++ db/reviews.js
@@ -1,4 +1,6 @@
 // AUTH TOKEN WILL BE REQUIRED IN ENDPOINTS
+// ADD MIDDLEWARE TO ENDPOINT THAT CHECKS IF USER HAS SUBMITTED A REVIEW ON BUSINESS ALREADY
+    // find unique where authorId: user id AND businessId: business id
 // create a review for user
 // update a user review
 // delete a user review
