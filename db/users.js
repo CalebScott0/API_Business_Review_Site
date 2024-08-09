ff6aceb6b2037f2a9a012e73efe051001ccc0b30
CalebScott0 <calebbenscott@gmail.com>
2024-08-08 21:55:18 -0500

    login and register routes running and tested

--- db/users.js
+++ db/users.js
@@ -8,9 +8,9 @@ const createUser = (userData) => {
 
 // if file gets too large with functions create a users.utils file and a users folder?
 
-// TEST OUT THE BELOW FUNCTIONS BY CREATING AN ASYNC FUNCTION AND CALLING THEM WITH AWAIT
-// console logging variable set to await function
-// (Call function to see console logged value lol)
+/* TEST OUT THE BELOW FUNCTIONS BY CREATING AN ASYNC FUNCTION AND CALLING THEM WITH AWAIT
+   console logging variable set to await function
+   (Call function to see console logged value lol) */
 
 // const userReviewCount = (id) => {
 //   return  prisma.review.count({
@@ -46,8 +46,11 @@ const createUser = (userData) => {
 // only get comments or reviews? no need to show comment data for user?
 
 const findUserById = (id) => {
-  /* call update user function here? this is the function that will be called 
+  /* call update user function here? change function async/await and use await on update function?
+       keep return on findUnique.
+     this is the function that will be called 
      in api/index.js to set req.user and subsequently will be used with "get/me" */
+
   return prisma.user.findUnique({
     where: { id },
     include: {
