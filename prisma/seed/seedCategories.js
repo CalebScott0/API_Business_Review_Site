2466c120620895c1400f70497a1d5d0956991c8f
CalebScott0 <calebbenscott@gmail.com>
2024-08-09 15:07:09 -0500

    seeding business categories

--- prisma/seed/seedCategories.js
+++ prisma/seed/seedCategories.js
@@ -6,17 +6,19 @@ const prisma = new PrismaClient();
 async function main() {
   console.log("Seeding business categories...");
   // change this to be all categories to show ranking by occurence in prisma query?
-  // const categoryArray = [];
-  // for (let i = 0; i < businessArr.length; i++) {
-  //   const splitCategories = businessArr[i].categories
-  //     ? businessArr[i].categories.split(", ")
-  //     : [];
-  //   for (let j = 0; j < splitCategories.length; j++) {
-  //     !categoryArray.includes(splitCategories[j]) &&
-  //       categoryArray.push(splitCategories[j]);
-  //   }
-  // }
-  // // console.log(categoryArray);
+  const categoryArray = [];
+  for (let i = 0; i < businessArr.length; i++) {
+    // split categories into array if business has categories property
+    const splitCategories = businessArr[i].categories
+      ? businessArr[i].categories.split(", ")
+      : [];
+    for (let j = 0; j < splitCategories.length; j++) {
+      // push category to array if not already included
+      !categoryArray.includes(splitCategories[j]) &&
+        categoryArray.push(splitCategories[j]);
+    }
+  }
+  console.log(categoryArray);
 }
 main()
   .then(async () => {
