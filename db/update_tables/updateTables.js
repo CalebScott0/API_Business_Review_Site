d5a0199ccb61d59c2ca7e855aecc707ec38beb8b
CalebScott0 <calebbenscott@gmail.com>
2024-08-13 12:53:30 -0500

    redoing prisma updates to speed up query functions

--- db/update_tables/updateTables.js
+++ db/update_tables/updateTables.js
@@ -1,21 +1,27 @@
 const {
   countBusinessReviews,
   averageBusinessStars,
-  countUserReviews,
-  countUserComments,
-  averageUserStars,
+  // countUserReviews,
+  // countUserComments,
+  // averageUserStars,
   roundHalf,
 } = require("./utils");
 
 const prisma = require("../index");
 
 async function main() {
-  console.log("Updating businesses with review count and average stars...");
   const businesses = await prisma.business.findMany({
+    where: {
+      reviewCount: 0,
+      stars: 0,
+    },
     select: {
       id: true,
     },
   });
+  console.log(
+    `Updating ${businesses.length} businesses with review count and average stars...`
+  );
   // for each business, update with review count and average stars rounded to nearest 0.5
   for (let i = 0; i < businesses.length; i++) {
     const reviews = await countBusinessReviews(businesses[i].id);
@@ -31,6 +37,7 @@ async function main() {
         stars: avgStars,
       },
     });
+    console.log(i);
   }
 
   console.log(
@@ -42,42 +49,44 @@ async function main() {
   );
   console.log("Businesses updated");
 
-  console.log("Updating users with review count and average stars...");
+  // will update specific user during get by id instead of the below
 
-  const users = await prisma.user.findMany({
-    select: {
-      id: true,
-    },
-  });
+  // console.log("Updating users with review count and average stars...");
 
-  // for each user, update with review & comment count and average stars rounded to nearest 0.5
-  for (let i = 0; i < users.length; i++) {
-    const reviews = await countUserReviews(users[i].id);
+  // const users = await prisma.user.findMany({
+  //   select: {
+  //     id: true,
+  //   },
+  // });
 
-    const comments = await countUserComments(users[i].id);
+  // // for each user, update with review & comment count and average stars rounded to nearest 0.5
+  // for (let i = 0; i < users.length; i++) {
+  //   const reviews = await countUserReviews(users[i].id);
 
-    const avgStars = roundHalf(
-      (await averageUserStars(users[i].id))._avg.stars
-    );
+  //   const comments = await countUserComments(users[i].id);
 
-    await prisma.user.update({
-      where: { id: users[i].id },
-      data: {
-        reviewCount: reviews,
-        commentCount: comments,
-        stars: avgStars,
-      },
-    });
-  }
+  //   const avgStars = roundHalf(
+  //     (await averageUserStars(users[i].id))._avg.stars
+  //   );
 
-  console.log(
-    await prisma.user.findUnique({
-      where: {
-        id: "ZXsESlVN4d0smeMivvxJtA",
-      },
-    })
-  );
-  console.log("Users updated");
+  //   await prisma.user.update({
+  //     where: { id: users[i].id },
+  //     data: {
+  //       reviewCount: reviews,
+  //       commentCount: comments,
+  //       stars: avgStars,
+  //     },
+  //   });
+  // }
+
+  // console.log(
+  //   await prisma.user.findUnique({
+  //     where: {
+  //       id: "ZXsESlVN4d0smeMivvxJtA",
+  //     },
+  //   })
+  // );
+  // console.log("Users updated");
 }
 
 main();
