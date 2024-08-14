9560dddbd8294e544977c56a4e17e9177cfedd75
CalebScott0 <calebbenscott@gmail.com>
2024-08-13 21:18:16 -0500

    redoing prisma updates to speed up query functions

--- db/update_tables/updateTables.js
+++ db/update_tables/updateTables.js
@@ -37,56 +37,19 @@ async function main() {
         stars: avgStars,
       },
     });
-    console.log(i);
+
+    // console log first, last and every 50 businesses to see progress
+    if (i === 0 || i % 50 === 0 || i === businesses.length - 1) {
+      console.log(
+        await prisma.business.findUnique({
+          where: { id: businesses[i].id },
+        })
+      );
+      console.log(`Updated business # ${i} / ${businesses.length}`);
+    }
   }
 
-  console.log(
-    await prisma.business.findUnique({
-      where: {
-        id: "3UdVZ2F978ZISKT7WlFrjg",
-      },
-    })
-  );
   console.log("Businesses updated");
-
-  // will update specific user during get by id instead of the below
-
-  // console.log("Updating users with review count and average stars...");
-
-  // const users = await prisma.user.findMany({
-  //   select: {
-  //     id: true,
-  //   },
-  // });
-
-  // // for each user, update with review & comment count and average stars rounded to nearest 0.5
-  // for (let i = 0; i < users.length; i++) {
-  //   const reviews = await countUserReviews(users[i].id);
-
-  //   const comments = await countUserComments(users[i].id);
-
-  //   const avgStars = roundHalf(
-  //     (await averageUserStars(users[i].id))._avg.stars
-  //   );
-
-  //   await prisma.user.update({
-  //     where: { id: users[i].id },
-  //     data: {
-  //       reviewCount: reviews,
-  //       commentCount: comments,
-  //       stars: avgStars,
-  //     },
-  //   });
-  // }
-
-  // console.log(
-  //   await prisma.user.findUnique({
-  //     where: {
-  //       id: "ZXsESlVN4d0smeMivvxJtA",
-  //     },
-  //   })
-  // );
-  // console.log("Users updated");
 }
 
 main();
