89f696cd799b7e0ef9454294f67c3ffd1c7f11ee
CalebScott0 <calebbenscott@gmail.com>
2024-08-15 13:13:34 -0500

    adding get all category and categories to display endpoints

--- project/db/categories.js
+++ project/db/categories.js
@@ -1,31 +1,5 @@
 const prisma = require("./index");
 
-// // given a category (string name) -> return count of all businsses with that category
-// const countCategories = (category) => {
-//   return prisma.categoryToBusiness.count({
-//     where: {
-//       categoryName: category,
-//     },
-//   });
-// };
-
-// /* update all categories
-//     - find all categories returning an array
-//     - loop through each category, returning count of all bussinesses with that category
-//     - update category that matches current array element with count of businesses*/
-// const updateCategories = async () => {
-//   const categories = await prisma.category.findMany();
-//   for (let i = 0; i < categories.length; i++) {
-//     const countBusinesses = await countCategories(categories[i].name);
-//     await prisma.category.update({
-//       where: { name: categories[i].name },
-//       data: {
-//         businessCount: countBusinesses,
-//       },
-//     });
-//   }
-// };
-
 // get all categories, returning id and name - ordered by name asc
 const getCategories = () => {
   // await updateCategories();
