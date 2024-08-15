0cbbed8943849894520d15a4a20bc4609ccfa0fb
CalebScott0 <calebbenscott@gmail.com>
2024-08-15 12:24:37 -0500

    adding get all category endpoint

---
+++ project/api/categories.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const categoryRouter = express.Router();
+
+const { getCategories } = require("../db/categories");
+
+// /api/categories
+categoryRouter.get("/", async (req, res, next) => {
+  try {
+    const categories = await getCategories();
+
+    res.send({ categories });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+module.exports = getCategories;
--- project/db/categories.js
+++ project/db/categories.js
@@ -1,36 +1,37 @@
 const prisma = require("./index");
 
-// given a category (string name) -> return count of all businsses with that category
-const countCategories = (category) => {
-  return prisma.categoryToBusiness.count({
-    where: {
-      categoryName: category,
-    },
-  });
-};
+// // given a category (string name) -> return count of all businsses with that category
+// const countCategories = (category) => {
+//   return prisma.categoryToBusiness.count({
+//     where: {
+//       categoryName: category,
+//     },
+//   });
+// };
 
-/* update all categories 
-    - find all categories returning an array
-    - loop through each category, returning count of all bussinesses with that category
-    - update category that matches current array element with count of businesses*/
-const updateCategories = async () => {
-  const categories = await prisma.category.findMany();
-  for (let i = 0; i < categories.length; i++) {
-    const countBusinesses = await countCategories(categories[i].name);
-    await prisma.category.update({
-      where: { name: categories[i].name },
-      data: {
-        businessCount: countBusinesses,
-      },
-    });
-  }
-};
+// /* update all categories
+//     - find all categories returning an array
+//     - loop through each category, returning count of all bussinesses with that category
+//     - update category that matches current array element with count of businesses*/
+// const updateCategories = async () => {
+//   const categories = await prisma.category.findMany();
+//   for (let i = 0; i < categories.length; i++) {
+//     const countBusinesses = await countCategories(categories[i].name);
+//     await prisma.category.update({
+//       where: { name: categories[i].name },
+//       data: {
+//         businessCount: countBusinesses,
+//       },
+//     });
+//   }
+// };
 
-// get all categories, ordered by count of businesses descending
+// get all categories, ordered by name asc
 const getCategories = async () => {
-  await updateCategories();
+  // await updateCategories();
   return prisma.category.findMany({
-    orderBy: { businessCount: "asc" },
+    orderBy: { name: "asc" },
+    select: { name: true },
   });
 };
 
