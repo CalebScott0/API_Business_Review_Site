9f4410c6ca2e2de581682c7ad53797b96ed0c6a8
CalebScott0 <calebbenscott@gmail.com>
2024-08-16 03:02:01 -0500

    added categories to display endpoint

--- project/api/categories.js
+++ project/api/categories.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const categoryRouter = express.Router();
 
-const getCategories = require("../db/categories");
+const { getCategories, getCategoriesToDisplay } = require("../db/categories");
 
 // /api/categories
 categoryRouter.get("/", async (req, res, next) => {
   try {
     const categories = await getCategories();
+
+    res.send({ categories });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+// /api/categories/display
+categoryRouter.get("/display", async (req, res, next) => {
+  try {
+    const categories = await getCategoriesToDisplay();
 
     res.send({ categories });
   } catch ({ name, message }) {
--- project/db/categories.js
+++ project/db/categories.js
@@ -1,8 +1,31 @@
 const prisma = require("./index");
+const categories = [
+  "Active Life",
+  "Arts & Entertainment",
+  "Automotive",
+  "Beauty & Spas",
+  "Education",
+  "Event Planning & Services",
+  "Financial Services",
+  "Food",
+  "Health & Medical",
+  "Home Services",
+  "Hotels & Travel",
+  "Local Services",
+  "Local Flavor",
+  "Mass Media",
+  "Nightlife",
+  "Pets",
+  "Professional Services",
+  "Public Services & Government",
+  "Real Estate",
+  "Religious Organizations",
+  "Restaurants",
+  "Shopping",
+];
 
 // get all categories, returning id and name - ordered by name asc
 const getCategories = () => {
-  // await updateCategories();
   return prisma.category.findMany({
     select: {
       id: true,
@@ -14,6 +37,15 @@ const getCategories = () => {
   });
 };
 
-const getCategoriesToDisplay = () => {};
+const getCategoriesToDisplay = () => {
+  return prisma.category.findMany({
+    where: {
+      name: { in: [...categories] },
+    },
+    orderBy: {
+      name: "asc",
+    },
+  });
+};
 
-module.exports = getCategories;
+module.exports = { getCategories, getCategoriesToDisplay };
