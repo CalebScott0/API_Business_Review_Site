2177f1bd6cacecca05bf372904cbe88595584d0a
CalebScott0 <calebbenscott@gmail.com>
2024-08-18 17:51:22 -0500

    setting up categories to be able to call business by categories endpoint

--- project/db/categories.js
+++ project/db/categories.js
@@ -3,11 +3,10 @@ const prisma = require("./index");
 // get all categories - ordered by count of businesses desc
 const getCategories = () => {
   return prisma.category.findMany({
-    // where: {
-    //   businessCount: { gte: 100 },
-    // },
+    where: {
+      businessCount: { gte: 100 },
+    },
     orderBy: { businessCount: "desc" },
-    take: 40,
   });
 };
 
