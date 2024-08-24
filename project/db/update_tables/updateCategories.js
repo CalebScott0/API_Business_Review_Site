33e4d0b515891f68d5d6e9f038780a42c4d22b24
CalebScott0 <calebbenscott@gmail.com>
2024-08-24 00:06:30 -0500

    updating tables

---
+++ project/db/update_tables/updateCategories.js
@@ -0,0 +1,22 @@
+const prisma = require("../index");
+
+async function main() {
+  // update category with count of businesses
+  const categories = await prisma.category.findMany();
+  for (let item of categories) {
+    const businessCount = await prisma.categoryToBusiness.count({
+      where: {
+        categoryName: item.name,
+      },
+    });
+    await prisma.category.update({
+      where: {
+        id: item.id,
+      },
+      data: { businessCount },
+    });
+  }
+  console.log(await prisma.category.findMany(), "Categories Seeded");
+}
+
+main();
