7ffd451f34873ffcae67e5dc77664a36c58eafbd
CalebScott0 <calebbenscott@gmail.com>
2024-09-16 16:18:31 -0500

    sped up get categories for front end

--- project/db/categories.js
+++ project/db/categories.js
@@ -3,6 +3,6 @@ const prisma = require("./index");
 // get all categories
 // - ordered by count of businesses desc
 const getCategories = () => {
-  return prisma.$queryRaw`SELECT DISTINCT "categoryName" FROM "CategoryToBusiness"`;
+  return prisma.$queryRaw`SELECT DISTINCT "categoryId", "categoryName" FROM "CategoryToBusiness"`;
 };
 module.exports = getCategories;
